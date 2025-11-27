9e64fd095cd1689cc80b3f8c1633f28e495b781c
solomon54 <solomontsehay54@gmail.com>
2025-11-27 20:42:56 +0300

    Include a ContextAPI for global state management

--- alx-project-0x05/context/CountContext.tsx
+++ alx-project-0x05/context/CountContext.tsx
@@ -1,12 +1,12 @@
 import { createContext, useContext, useState, ReactNode }from "react";
 
-interface countContextPropes {
+    interface CountContextProps {
     count: number,
     increment: () => void;
     decrement: () => void;
 }
 
-export const CountContext = createContext<countContextPropes | undefined>(undefined);
+export const CountContext = createContext<CountContextProps | undefined>(undefined);
 
 export const CountProvider = ({ children}: { children: ReactNode}) => {
 const [count, setCount] = useState<number>(0);
